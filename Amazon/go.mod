ea8e41e1a684e9bea337836fb6fa639eccd4ef1b
amol <gajare147@gmail.com>
2024-06-18 23:47:54 +0530

    removed src

---
+++ Amazon/go.mod
@@ -0,0 +1,3 @@
+module Amazon
+
+go 1.22.4
--- Amazon/user/internal/handler/handler.go
+++ Amazon/user/internal/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"/Amazon/user/internal/models/"
 )
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
