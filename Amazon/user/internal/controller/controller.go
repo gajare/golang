51fcdba811060df70f4fb2192f31c1fc7fdb68b9
amol <gajare147@gmail.com>
2024-06-19 01:06:07 -0800

    add controller

--- Amazon/user/internal/controller/controller.go
+++ Amazon/user/internal/controller/controller.go
@@ -1,26 +1,23 @@
-package handler
+package controller
 
-import (
-	"Amazon/user/internal/controller"
-	"net/http"
-)
+import "net/http"
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
-	controller.ListUsers(w, r)
+
 }
 
 func Getuser(w http.ResponseWriter, r *http.Request) {
-	controller.Getuser(w, r)
+
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	controller.CreateUser(w, r)
+
 }
 
 func UpadateUser(w http.ResponseWriter, r *http.Request) {
-	controller.UpadateUser(w, r)
+
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	controller.DeleteUser(w, r)
+
 }
