828f5eb5d147d7830bbde523a4c9f21db6663b4f
amol <gajare147@gmail.com>
2024-06-19 02:39:02 -0800

    service.go file  updated

--- Amazon/user/internal/service/service.go
+++ Amazon/user/internal/service/service.go
@@ -1,5 +1,13 @@
 package service
 
+import "Amazon/user/internal/models"
+
 // The service layer typically encapsulates business logic that is reusable across different controllers or even across multiple microservices.
 
+func CreateUser(user models.User) (*models.User, error) {
+	return &user, nil
+}
 
+func UpadateUser(user_id string, user models.User) (*models.User,error) {
+	return &user, nil
+}
